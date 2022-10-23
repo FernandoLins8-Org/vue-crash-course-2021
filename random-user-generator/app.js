13f9dee0430d8919565bc0d1b061eb8d8db11bd0
Fernando Lins <fernandolins088@gmail.com>
2022-10-23 02:20:34 -0300

    Add random user generator project

---
+++ random-user-generator/app.js
@@ -0,0 +1,29 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      firstName: '',
+      lastName: '',
+      email: '',
+      gender: '',
+      picture: ''
+    }
+  },
+  methods: {
+    async getUser() {
+      const res = await fetch('https://randomuser.me/api')
+      const { results } = await res.json()
+
+      this.firstName = results[0].name.first,
+      this.lastName = results[0].name.lat,
+      this.email = results[0].email,
+      this.gender = results[0].gender,
+      this.picture = results[0].picture.large
+    }
+  },
+  created() {
+    this.getUser()
+  }
+})
+
+app.mount('#app')
+
